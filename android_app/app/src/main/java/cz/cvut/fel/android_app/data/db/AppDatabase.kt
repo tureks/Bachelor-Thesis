05744422630a5b831829ce4b43948218afa93365
Štěpán Turek <step.turek@gmail.com>
2026-04-22 00:58:29 +0200

    feat: Add singleton to access database

--- android_app/app/src/main/java/cz/cvut/fel/android_app/data/db/AppDatabase.kt
+++ android_app/app/src/main/java/cz/cvut/fel/android_app/data/db/AppDatabase.kt
@@ -1,11 +1,13 @@
 package cz.cvut.fel.android_app.data.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import cz.cvut.fel.android_app.data.device.Device
 import cz.cvut.fel.android_app.data.device.DeviceDao
-import cz.cvut.fel.android_app.data.measurement.StreamMeasurementDao
 import cz.cvut.fel.android_app.data.measurement.StreamMeasurement
+import cz.cvut.fel.android_app.data.measurement.StreamMeasurementDao
 import cz.cvut.fel.android_app.data.stream_segment.StreamSegment
 import cz.cvut.fel.android_app.data.stream_segment.StreamSegmentDao
 import cz.cvut.fel.android_app.data.velocity_point.VelocityPoint
@@ -20,9 +22,21 @@ import cz.cvut.fel.android_app.data.velocity_point.VelocityPointDao
     ],
     version = 1
 )
-abstract class Database : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun deviceDao(): DeviceDao
     abstract fun measurementDao(): StreamMeasurementDao
     abstract fun streamSegmentDao(): StreamSegmentDao
     abstract fun velocityPointDao(): VelocityPointDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(context, AppDatabase::class.java, "app_database")
+                    .build()
+                    .also { INSTANCE = it }
+            }
+    }
 }

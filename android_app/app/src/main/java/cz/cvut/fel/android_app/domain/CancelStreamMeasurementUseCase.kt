c9fc57178fbf9fee37574159118969f934bd7af9
Štěpán Turek <step.turek@gmail.com>
2026-04-25 11:13:42 +0200

    feat: Deletition of stream measurement

--- android_app/app/src/main/java/cz/cvut/fel/android_app/domain/CancelStreamMeasurementUseCase.kt
+++ android_app/app/src/main/java/cz/cvut/fel/android_app/domain/CancelStreamMeasurementUseCase.kt
@@ -1,4 +1,16 @@
 package cz.cvut.fel.android_app.domain
 
-class CancelStreamMeasurementUseCase {
+import cz.cvut.fel.android_app.domain.repository.StreamMeasurementRepository
+
+class CancelStreamMeasurementUseCase(
+    private val repository: StreamMeasurementRepository
+) {
+    /**
+     * Discards the current draft measurement.
+     */
+    suspend operator fun invoke() {
+        repository.getDraft()?.let {
+            repository.delete(it)
+        }
+    }
 }
--- android_app/app/src/main/java/cz/cvut/fel/android_app/domain/DeleteStreamMeasurementUseCase.kt
+++ android_app/app/src/main/java/cz/cvut/fel/android_app/domain/DeleteStreamMeasurementUseCase.kt
@@ -1,4 +1,17 @@
 package cz.cvut.fel.android_app.domain
 
-class DeleteStreamMeasurementUseCase {
+import cz.cvut.fel.android_app.domain.repository.StreamMeasurementRepository
+
+class DeleteStreamMeasurementUseCase(
+    private val repository: StreamMeasurementRepository
+) {
+    /**
+     * Deletes a specific measurement by its ID.
+     * Note: Deletion is cascaded to all child segments and velocity points
+     */
+    suspend operator fun invoke(id: Int) {
+        repository.getById(id)?.let {
+            repository.delete(it)
+        }
+    }
 }

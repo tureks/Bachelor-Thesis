0525e637294d59fb9bd10e9e69d4ba72ba9465a9
Štěpán Turek <step.turek@gmail.com>
2026-04-25 14:54:19 +0200

    feat: Added update stream metadata use case

--- android_app/app/src/main/java/cz/cvut/fel/android_app/domain/UpdateStreamMeasurementUseCase.kt
+++ android_app/app/src/main/java/cz/cvut/fel/android_app/domain/UpdateStreamMeasurementUseCase.kt
@@ -1,4 +1,27 @@
 package cz.cvut.fel.android_app.domain
 
-class UpdateStreamMeasurementUseCase {
+import cz.cvut.fel.android_app.domain.repository.StreamMeasurementRepository
+
+class UpdateStreamMeasurementUseCase(
+    private val repository: StreamMeasurementRepository
+) {
+    /**
+     * Updates the metadata of a stream measurement.
+     */
+    suspend operator fun invoke(
+        measurementId: Int,
+        name: String,
+        location: String?,
+        note: String?
+    ) {
+        val measurement = repository.getById(measurementId) ?: return
+        
+        val updatedMeasurement = measurement.copy(
+            name = name,
+            location = location,
+            note = note
+        )
+        
+        repository.update(updatedMeasurement)
+    }
 }
